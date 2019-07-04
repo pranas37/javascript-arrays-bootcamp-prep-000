8087122d3f9c006d607bb440efc0398fcf197ba9
benevolent-listener-0694 <pranas_29@yahoo.co.uk>
2019-07-04 13:52:56 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -36,6 +36,7 @@ function destructivelyAddElementToEndOfArray(array,element){
     return (3)
   }
   function RemoveElementFromBeginningOfArray(array){
-     return ([2,3] )
+     array.unshift ([2,3] )
+     return array
   }
   
