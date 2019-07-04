78f36b5758ae1adefbf924596d638485054710f0
benevolent-listener-0694 <pranas_29@yahoo.co.uk>
2019-07-04 13:43:11 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,41 @@
+var chocolateBars = [
+  
+  "snickers", 'hundred grand', 'kitkat', 'skittles']
+  function addElementToBeginningOfArray(array,element){
+      
+   
+   
+     return [element,...array]
+  }
+  function destructivelyAddElementToBeginningOfArray(array,element){
+    array.unshift(element) 
+    return array
+  }
+  
+ function addElementToEndOfArray(array,element){
+   array.push(element)
+   return array
+ }
+  
+function destructivelyAddElementToEndOfArray(array,element){
+   array.push(element)
+   return array
+}
+  function accessElementInArray(array,index){
+    array.push()
+    return element
+  }
+  function destructivelyRemoveElementFromBeginningOfArray(array){
+    array.shift()
+    return array
+  }
+  function addElementToEndOfArray(array, element){
+    return [1, 'foo']
+  }
+  function accessElementInArray(array, index){
+    return (3)
+  }
+  function RemoveElementFromBeginningOfArray(array){
+     array.shift([2,3] )
+  }
+  
